bc5eca757c7d66509b220123a426a3e1e3108615
Peter Zandbergen <peter.zandbergen@myhops.com>
2020-02-18 01:46:45 +0100

    fix disappearing serial port handling

--- message.go
+++ message.go
@@ -29,6 +29,7 @@ var (
 	ResponseOK        = errors.New("response found")
 	ErrBufferTooShort = errors.New("buffer must be at least 10 long")
 	ErrBadType        = errors.New("bad type")
+	ErrPortReadError = errors.New("port read error")
 )
 
 type Request [requestLen]byte
@@ -151,6 +152,17 @@ func NewResponse(p []byte) (interface{}, error) {
 // b needs to be at lease 10 bytes long or an error will be returned.
 // n will always be 10 for now.
 func ReadResponseBytes(ctx context.Context, r io.Reader, b []byte) (n int, err error) {
+	// read treats a zero bytes read as an error
+	read := func(n int, err error) (int, error) {
+		if err != nil {
+			return 0, err
+		}
+		if n == 0 {
+			return 0, ErrPortReadError
+		}
+		return n, err
+	}
+
 	if len(b) < responseLen {
 		return 0, ErrBufferTooShort
 	}
@@ -158,9 +170,9 @@ func ReadResponseBytes(ctx context.Context, r io.Reader, b []byte) (n int, err e
 	for {
 		var p1 [1]byte
 		for {
-			n, err := r.Read(p1[:])
-			if err != nil || n == 0 {
-				return n, err
+			_, err := read(r.Read(p1[:]))
+			if err != nil {
+				return 0, err
 			}
 			if p1[0] == head {
 				b[0] = head
@@ -168,9 +180,9 @@ func ReadResponseBytes(ctx context.Context, r io.Reader, b []byte) (n int, err e
 			}
 		}
 		// Read next byte
-		n, err := r.Read(p1[:])
-		if err != nil || n == 0 {
-			return n, err
+		_, err := read(r.Read(p1[:]))
+		if err != nil {
+			return 0, err
 		}
 		if isResponseID(p1[0]) {
 			b[1] = p1[0]
@@ -180,7 +192,7 @@ func ReadResponseBytes(ctx context.Context, r io.Reader, b []byte) (n int, err e
 	// head and commandID read.
 	i := 2
 	for i < 10 {
-		n, err := r.Read(b[i:responseLen])
+		n, err := read(r.Read(b[i:responseLen]))
 		if err != nil {
 			return i + n, err
 		}
